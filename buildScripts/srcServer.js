c38fd208f512116d98fc384b54d728718fa190cc
Paul Cavanagh <paulcavanagh@Pauls-MacBook-Pro.local>
2017-11-08 12:41:19 +0000

    HTTP, Mock API

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  // Hard coding for simplicity. Pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName": "Paul", "lastName":"Cavanagh", "email": "pol@gmail.com"},
+    {"id": 2, "firstName": "Anita", "lastName":"Cavanagh", "email": "aod@gmail.com"},
+    {"id": 3, "firstName": "Ruby", "lastName":"Cavanagh", "email": "dol@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err)
--- src/index.test.js
+++ src/index.test.js
@@ -15,11 +15,11 @@ describe('Our second test', function(){
 });
 
 describe('index.html', () => {
-  it('should say hello', (done)=> {
+  it('should say he that says User', (done)=> {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window){
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.be.equal("Hello Paul!");  
+      expect(h1.innerHTML).to.be.equal("Users");  
       done();
       window.close();
     });
