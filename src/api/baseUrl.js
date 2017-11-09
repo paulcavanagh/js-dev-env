28a021435d4482842d5f26e572fcc1ff08d91b38
Paul Cavanagh <paulcavanagh@Pauls-MacBook-Pro.local>
2017-11-09 12:21:36 +0000

    Course completed

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl(){
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://calm-sea-88191.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url){
